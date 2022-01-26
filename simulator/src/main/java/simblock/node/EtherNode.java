d8cdd0ed87fa99a7774a4030bc33c79e3ebca696
bella meng <dancingm1998@gmail.com>
2022-01-26 15:56:06 +0800

    EtherNode

---
+++ simulator/src/main/java/simblock/node/EtherNode.java
@@ -0,0 +1,12 @@
+package simblock.node;
+
+public class EtherNode extends Node{
+
+	public EtherNode(int nodeID, int numConnection, int region, long miningPower, String routingTableName,
+			String consensusAlgoName, boolean useCBR, boolean isChurnNode) {
+		
+		super(nodeID, numConnection, region, miningPower, routingTableName, consensusAlgoName, useCBR, isChurnNode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
