82f7b26695ccaee32f908a956773094e55594444
bella meng <dancingm1998@gmail.com>
2022-01-26 15:56:36 +0800

    EtherBlock

---
+++ simulator/src/main/java/simblock/block/EtherBlock.java
@@ -0,0 +1,41 @@
+package simblock.block;
+
+import java.util.ArrayList;
+
+//import simblock.block.Block;
+import simblock.node.Node;
+import simblock.transaction.Transaction;
+
+public class EtherBlock extends Block {
+	
+	private int gasLimit;
+	private int gasUsed;
+	
+	
+	public EtherBlock(Block parent, Node minter, long time, ArrayList<Transaction> transactions) {
+		super(parent, minter, time, transactions);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getGasLimit() {
+		return gasLimit;
+	}
+
+
+	public void setGasLimit(int gasLimit) {
+		this.gasLimit = gasLimit;
+	}
+
+
+	public int getGasUsed() {
+		return gasUsed;
+	}
+
+
+	public void setGasUsed(int gasUsed) {
+		this.gasUsed = gasUsed;
+	}
+	
+	
+}
