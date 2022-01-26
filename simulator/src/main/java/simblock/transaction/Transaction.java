ce82ab47ce9180372d479a0c8400a6acd58e270d
bella meng <dancingm1998@gmail.com>
2022-01-26 15:38:29 +0800

    deleted sender and receiver in transaction since not needed

--- simulator/src/main/java/simblock/transaction/Transaction.java
+++ simulator/src/main/java/simblock/transaction/Transaction.java
@@ -1,16 +1,10 @@
 package simblock.transaction;
 
-import simblock.node.Node;
-
 public class Transaction {
-	private Node sender;
-	private Node receiver;
 	private int transactionFee;
 	private int value;
 	
-	public Transaction(Node sender, Node receiver, int value, int transactionFee) {
-		this.sender = sender;
-		this.receiver = receiver;
+	public Transaction(int value, int transactionFee) {
 		this.transactionFee = transactionFee;
 	}
 	
@@ -18,14 +12,6 @@ public class Transaction {
 		return value;
 	}
 
-	public Node getSender() {
-		return this.sender;
-	}
-	
-	public Node getReceiver() {
-		return this.receiver;
-	}
-	
 	public int getTransactionFee() {
 		return this.transactionFee;
 	}
